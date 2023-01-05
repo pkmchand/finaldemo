092b69e07361fab4edfd3ee924a3e24644665c17
pkmchand <120459510+pkmchand@users.noreply.github.com>
2023-01-05 16:34:48 +0530

    updated cors_1

--- index.js
+++ index.js
@@ -39,13 +39,14 @@ const corsOpts = {
 };
 
 app.use(cors());
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
+app.options('*', cors());
+// app.use(function(req, res, next) {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+//   next();
+// });
 
 
 // adding morgan to log HTTP requests
