9719bfd7974b6c3cfa7ce47012da5c0ba856bf72
pkmchand <120459510+pkmchand@users.noreply.github.com>
2023-01-06 12:28:49 +0530

    updated cors header

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ app.use(cors(corsOptionsDelegate));
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
